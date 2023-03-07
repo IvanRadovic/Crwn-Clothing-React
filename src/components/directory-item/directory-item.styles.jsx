627ce733f2d0d07780abe239cab3ce07f0ee3c98
Ivan <ivan.radovic@acme.co.me>
2023-03-07 09:29:07 +0100

    change color of text

--- src/components/directory-item/directory-item.styles.jsx
+++ src/components/directory-item/directory-item.styles.jsx
@@ -16,7 +16,7 @@
       align-items: center;
       justify-content: center;
       border: 1px solid black;
-      background-color: white;
+      background-color: black;
       opacity: 0.7;
       position: absolute;
 
@@ -24,12 +24,13 @@
         font-weight: bold;
         margin: 0 6px 0;
         font-size: 22px;
-        color: #4a4a4a;
+        color: white;
         text-transform:uppercase;
       }
 
       p {
         font-weight: lighter;
+        color:white;
         font-size: 16px;
       }
 
